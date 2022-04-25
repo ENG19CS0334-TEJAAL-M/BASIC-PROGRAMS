299e3ef9ddafb6ae913351ba6cbd9c6f479f7ece
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2022-04-25 22:19:10 +0530

    Create Mahasena_AMR15A.c

---
+++ codechef programs/Mahasena_AMR15A.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	
+	int n[100],i,even=0,odd=0,t;
+
+	scanf("%d\n",&t);
+	for(i=0;i<t;i++)
+	{
+	    scanf("%d ",&n[i]);
+	}
+	for(int j=0;j<t;j++)
+	{
+	    if(n[j]%2==0)
+	    {
+	        even++;
+	    }
+	    else
+	    {
+	       odd++;
+	    }
+	}
+	if(even>odd)
+	{
+	    printf("READY FOR BATTLE");
+	}
+	else
+	{
+	    printf("NOT READY");
+	}
+	
+	return 0;
+}
+
